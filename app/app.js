d26c2d4a573e8fd5096d52d5eee2a8be32cceb3c
rsstdd <rosstod@gmail.com>
2016-11-05 13:26:47 -0700

    Define starting structure

--- app/app.js
+++ app/app.js
@@ -1,3 +1,34 @@
-import angular from 'angular'
+import angular from 'angular';
+import angularMaterialize from 'angular-materialize';
 
-angular.module('my-app', []);
+import uiRouter from 'angular-ui-router';
+
+angular.module('my-app', [angularMaterialize, uiRouter]);
+
+  // .config(['$stateProvider', ($stateProvider) => {
+  //   $stateProvider
+  //     .state('home', {
+  //       url: '',
+  //       templateUrl: 'views/home.html'
+  //     })
+  //     .state('main', {
+  //       url: '/',
+  //       templateUrl: 'views/home.html'
+  //     })
+  //     .state('diagonalley', {
+  //       url: '/diagonalley',
+  //       templateUrl: 'views/diagonalley.html'
+  //     })
+  //     .state('user', {
+  //       url: '/user',
+  //       templateUrl: 'views/user/user.html'
+  //     })
+  //     .state('user.profile', {
+  //       url: '/profile',
+  //       templateUrl: 'views/user/profile.html'
+  //     })
+  //     .state('user.account', {
+  //       url: '/account',
+  //       templateUrl: 'views/user/account.html'
+  //     });
+  // }]);
